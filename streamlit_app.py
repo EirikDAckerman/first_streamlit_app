1238a42079dbf2cbe9359c16760280bf96398715
EirikDAckerman <53865613+EirikDAckerman@users.noreply.github.com>
2023-08-02 08:53:07 -0400

    Update streamlit_app.py
    Add new section to add fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+# New Section to add fruit
+streamlit.text("What fruit would you like to add?")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 # This will not work correctly, but go with it for now
